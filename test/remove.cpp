2b157694f5dbd64268dc04dd475f16d3e242acae
ogiwara <yushiogiwara@gmail.com>
2020-09-24 17:10:59 +0900

    add tests to remove test

--- test/remove.cpp
+++ test/remove.cpp
@@ -1,11 +1,36 @@
 #include <gtest/gtest.h>
 #include "../include/operation.h"
+#include "sample.h"
 
 using namespace masstree;
 
 class RemoveTest: public ::testing::Test{};
 
-TEST(RemoveTest, left_most){
+//region 削除後のBorderNodeに要素が残るケース
+
+TEST(RemoveTest, not_empty_border){
+  auto b = new BorderNode;
+  b->version.is_root = true;
+
+  b->key_len[0] = 1;
+  b->key_slice[0] = ONE;
+  b->key_len[1] = 1;
+  b->key_slice[1] = TWO;
+  b->key_len[2] = 1;
+  b->key_slice[2] = THREE;
+
+  Key k({TWO}, 1);
+  auto pair = remove(b, k, nullptr, 0);
+  EXPECT_TRUE(pair.second == b);
+  EXPECT_EQ(b->key_slice[0], ONE);
+  EXPECT_EQ(b->key_slice[1], THREE);
+}
+
+//endregion
+
+//region Treeの中で、一番端のBorder Nodeが削除される場合
+
+TEST(RemoveTest, left_most_1){
   auto a = new InteriorNode;
   auto b = new InteriorNode;
   auto c = new BorderNode;
@@ -38,6 +63,46 @@ TEST(RemoveTest, left_most){
   EXPECT_TRUE(a->child[0] == d);
 }
 
+TEST(RemoveTest, left_most_2){
+  auto a = new InteriorNode;
+  auto b = new InteriorNode;
+  auto c = new BorderNode;
+  auto d = new BorderNode;
+  auto e = new BorderNode;
+
+  a->key_slice[0] = 15;
+  a->version.is_root = true;
+  a->child[0] = b;
+  a->n_keys = 1;
+  b->key_slice[0] = 13;
+  b->key_slice[1] = 14;
+  b->child[0] = c;
+  b->child[1] = d;
+  b->child[2] = e;
+  b->n_keys = 2;
+  c->key_len[0] = 1;
+  c->key_slice[0] = 12;
+  d->key_len[0] = 1;
+  d->key_slice[0] = 13;
+  e->key_len[0] = 1;
+  e->key_slice[0] = 14;
+
+  e->parent = b;
+  e->prev = d;
+  d->parent = b;
+  d->next = e;
+  d->prev = c;
+  c->parent = b;
+  c->next = d;
+  b->parent = a;
+
+  Key k({12}, 1);
+  auto pair = remove(a, k, nullptr, 0);
+  EXPECT_TRUE(pair.second == a);
+  EXPECT_EQ(b->n_keys, 1);
+  EXPECT_EQ(b->key_slice[0], 14);
+}
+
 TEST(RemoveTest, right_most){
   auto a = new InteriorNode;
   auto b = new InteriorNode;
@@ -68,3 +133,142 @@ TEST(RemoveTest, right_most){
   EXPECT_TRUE(pair.second == a);
   EXPECT_TRUE(a->child[1] == c);
 }
+
+TEST(RemoveTest, right_most_2){
+  auto a = new InteriorNode;
+  auto b = new InteriorNode;
+  auto c = new BorderNode;
+  auto d = new BorderNode;
+  auto e = new BorderNode;
+
+  a->key_slice[0] = 20;
+  a->version.is_root = true;
+  a->child[1] = b;
+  a->n_keys = 1;
+  b->key_slice[0] = 22;
+  b->key_slice[1] = 24;
+  b->child[0] = c;
+  b->child[1] = d;
+  b->child[2] = e;
+  b->n_keys = 2;
+  c->key_len[0] = 1;
+  c->key_slice[0] = 20;
+  d->key_len[0] = 1;
+  d->key_slice[0] = 22;
+  d->key_len[1] = 1;
+  d->key_slice[1] = 23;
+  e->key_len[0] = 1;
+  e->key_slice[0] = 24;
+
+  e->parent = b;
+  e->prev = d;
+  d->parent = b;
+  d->next = e;
+  d->prev = c;
+  c->parent = b;
+  c->next = d;
+  b->parent = a;
+
+  Key k({24}, 1);
+  auto pair = remove(a, k, nullptr, 0);
+  EXPECT_EQ(b->n_keys, 1);
+  EXPECT_EQ(b->key_slice[0], 22);
+}
+
+//endregion
+
+//region treeの中で、端以外に位置するBorderNodeをdelete
+TEST(RemoveTest, middle1){
+  auto a = new InteriorNode;
+  auto b = new InteriorNode;
+  auto c = new BorderNode;
+  auto d = new BorderNode;
+
+  a->key_slice[0] = 25;
+  a->version.is_root = true;
+  a->child[0] = b;
+  a->n_keys = 1;
+  b->key_slice[0] = 15;
+  b->child[0] = c;
+  b->child[1] = d;
+  b->n_keys = 1;
+  c->key_len[0] = 1;
+  c->key_slice[0] = 5;
+  d->key_len[0] = 1;
+  d->key_slice[0] = 15;
+
+  d->parent = b;
+  d->prev = c;
+  c->parent = b;
+  c->next = d;
+  b->parent = a;
+
+  Key k({15}, 1);
+  auto pair = remove(a, k, nullptr, 0);
+  EXPECT_TRUE(pair.second == a);
+  EXPECT_TRUE(a->child[0] == c);
+  EXPECT_TRUE(c->parent == a);
+}
+
+TEST(RemoveTest, middle2){
+  auto a = new InteriorNode;
+  auto b = new InteriorNode;
+  auto c = new BorderNode;
+  auto d = new BorderNode;
+  auto e = new BorderNode;
+  auto f = new BorderNode;
+  auto g = new BorderNode;
+
+  a->key_slice[0] = 20;
+  a->version.is_root = true;
+  a->child[1] = b;
+  a->n_keys = 1;
+  b->key_slice[0] = 21;
+  b->key_slice[1] = 22;
+  b->key_slice[2] = 23;
+  b->key_slice[3] = 24;
+  b->child[0] = c;
+  b->child[1] = d;
+  b->child[2] = e;
+  b->child[3] = f;
+  b->child[4] = g;
+  b->n_keys = 4;
+  c->key_len[0] = 1;
+  c->key_slice[0] = 20;
+  d->key_len[0] = 1;
+  d->key_slice[0] = 21;
+  e->key_len[0] = 1;
+  e->key_slice[0] = 22;
+  f->key_len[0] = 1;
+  f->key_slice[0] = 23;
+  g->key_len[0] = 1;
+  g->key_slice[0] = 24;
+
+  g->parent = b;
+  g->prev = f;
+  f->parent = b;
+  f->next = g;
+  f->prev = e;
+  e->parent = b;
+  e->next = f;
+  e->prev = d;
+  d->parent = b;
+  d->next = e;
+  d->prev = c;
+  c->parent = b;
+  c->next = d;
+  b->parent = a;
+
+  Key k({22}, 1);
+  auto pair = remove(a, k, nullptr, 0);
+  EXPECT_EQ(b->n_keys, 3);
+  EXPECT_EQ(b->key_slice[0], 21);
+  EXPECT_EQ(b->key_slice[1], 23);
+  EXPECT_EQ(b->key_slice[2], 24);
+  EXPECT_TRUE(b->child[2] == f);
+}
+//endregion
+
+//region root付近
+
+//endregion
