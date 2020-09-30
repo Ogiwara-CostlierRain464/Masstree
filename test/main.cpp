e40a5d571a77deed3729fb45aab8c3725ae59156
ogiwara <yushiogiwara@gmail.com>
2020-09-30 16:01:29 +0900

    update unit test

--- test/main.cpp
+++ test/main.cpp
@@ -4,18 +4,35 @@ using namespace std;
 
 class Unit: public ::testing::Test{};
 
-template<typename T>
-void pop_front(std::vector<T> &v)
-{
-  if (!v.empty()) {
-    v.erase(v.begin());
+// atomicにおいては、これは有効ではない、しっかりセッターゲッターを分ける必要性が出てくる。
+class MyArray{
+private:
+  int *ptr;
+  int size;
+
+public:
+  explicit MyArray(const int* p = nullptr, int s = 0){
+    size = s;
+    ptr = NULL;
+    if (s != 0) {
+      ptr = new int[s];
+      for (int i = 0; i < s; i++)
+        ptr[i] = p[i];
+    }
   }
-}
+
+  int& operator[](std::size_t index){
+    return ptr[index];
+  }
+};
+
 
 TEST_F(Unit, init){
-  vector<int> a = {1,2,3};
-  pop_front(a);
-  EXPECT_EQ(a.size(), 2);
+  int a[] = {1,2,3};
+  MyArray arr(a, 3);
+
+  arr[2] = 6;
+  EXPECT_EQ(arr[2], 0);
 }
 
 int main(int argc, char **argv){
