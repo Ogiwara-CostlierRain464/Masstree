369f211e24ad4281f1a9ea70eb2c74fdcef33a36
ogiwara <yushiogiwara@gmail.com>
2020-10-11 21:08:52 +0900

    add multi_insert_border_layer0_test

--- test/large.cpp
+++ test/large.cpp
@@ -13,6 +13,12 @@ using namespace masstree;
 
 class LargeTest: public ::testing::Test{};
 
+Key *make_1layer_key(){
+  KeySlice slice = rand() % 3;
+  auto size = (rand() % 8) + 1;
+  return new Key({slice}, size);
+}
+
 Key *make_key(){
   std::vector<KeySlice> vec{};
   size_t slices_len = (rand() % 2) + 1;
@@ -132,8 +138,48 @@ TEST(LargeTest, DISABLED_random_op){
   Alloc::reset();
 }
 
+TEST(LargeTest, multi_insert_border_layer0_test){
+  auto seed = time(0);
+  srand(seed);
+
+  for(size_t i = 0; i < 1000000; ++i){
+
+    std::atomic<Node*> root = nullptr;
+    std::atomic_bool ready{false};
+
+    auto w1 = [&root, &ready](){
+      while (!ready){ _mm_pause(); }
+
+      GC gc{};
+      for(size_t i = 0; i < 15; ++i){
+        auto k = make_1layer_key();
+        root = put_at_layer0(root, *k, new Value(k->remainLength(0)), gc);
+      }
+    };
+
+    auto w2 = [&root, &ready](){
+      while (!ready){ _mm_pause(); }
+
+      for(size_t i = 0; i < 15; ++i){
+        auto k = make_1layer_key();
+        auto p = get(root, *k);
+        if(p != nullptr){
+          auto body = p->getBody();
+          ASSERT_TRUE(1 <= body and body <= 8);
+        }
+      }
+    };
+
+    std::thread a(w1);
+    std::thread b(w2);
+    ready = true;
+    a.join();
+    b.join();
+  }
+}
+
 TEST(LargeTest, DISABLED_multi_insert_border_test){
-  auto seed = 1602407569;
+  auto seed = 1602407569; // UNSTABLE errorが出るseed
   srand(seed);
 
   for(size_t i = 0; i < 1000000; ++i){
